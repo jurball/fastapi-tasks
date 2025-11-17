e59e781766b5b92c0421cdfe27a2e13f2f19d6c7
jurball <42448176+jurball@users.noreply.github.com>
2025-11-17 22:31:54 +0300

    Create repository.py

---
+++ repository.py
@@ -0,0 +1,26 @@
+from sqlalchemy import select
+
+from database import new_session, TaskOrm
+from main import STaskAdd
+
+class TaskRepository:
+    @classmethod
+    async def add_one(cls, data: STaskAdd) -> int:
+        async with new_session() as session:
+            task_dict = data.model_dump() # превращаем в виде словаря
+
+            task = TaskOrm(**task_dict) # создать таску
+            session.add(task) # выполняем транзакцию
+            await session.flush() # получит id
+            await session.commit() # выполнить в БД
+
+            return task.id
+
+    @classmethod
+    async def find_all(cls):
+        async with new_session() as session:
+            query = select(TaskOrm) # запрос
+            result = await session.execute(query) # исполнить запрос
+            task_models = result.scalars().all() # получить все объект итерации
+            return task_models
+
