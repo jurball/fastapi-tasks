f3086325047ccd98a34f167b9ef9f4ea2a732b37
jurball <42448176+jurball@users.noreply.github.com>
2025-11-17 22:47:36 +0300

    Update database.py
    Переименовал имя файла sqlite

--- database.py
+++ database.py
@@ -1,9 +1,11 @@
+from typing import Optional
+
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, Mapped
 from sqlalchemy.testing.schema import mapped_column
 
 engine = create_async_engine( # асинхронный движок для работы с БД
-    "sqlite+aiosqlite:///:tasks.db"
+    "sqlite+aiosqlite:///tasks.db"
 )
 
 new_session = async_sessionmaker(engine, expire_on_commit=False) # открытие транзакции БД
@@ -16,7 +18,7 @@ class TaskOrm(Model): # Объект модели
 
     id: Mapped[int] = mapped_column(primary_key=True) # первичный ключ
     name: Mapped[str]
-    description: Mapped[str | None]
+    description: Mapped[Optional[str]]
 
 async def create_tables(): # функция для создания таблиц
     async with engine.begin() as conn: # обращаться к engine
