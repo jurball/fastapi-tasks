8c6fa2d12aa3f347ce32e149f2cf10334d371deb
jurball <42448176+jurball@users.noreply.github.com>
2025-11-17 21:42:09 +0300

    create post endpoint task

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-from typing import Optional
+from typing import Optional, Annotated
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
 
 app = FastAPI()
 
-class Task(BaseModel): # сущность таски
+class STaskAdd(BaseModel): # сущность таски
     name: str
     description: Optional[str] = None # как необязательный
 
+tasks = []
+
+@app.post("/tasks")
+async def add_task(
+        task: Annotated[STaskAdd, Depends()]
+):
+    tasks.append(task)
+    return { "ok": True }
+
 @app.get("/tasks")
 def get_tasks():
-    task = Task(name="New Task")
-    return { "data": task }
+    return { "data": tasks }
