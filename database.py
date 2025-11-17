7bce4c6be9d3d201d5c4e6cd8027855400bec674
jurball <42448176+jurball@users.noreply.github.com>
2025-11-17 22:11:08 +0300

    Create database.py

---
+++ database.py
@@ -0,0 +1,27 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase, Mapped
+from sqlalchemy.testing.schema import mapped_column
+
+engine = create_async_engine( # асинхронный движок для работы с БД
+    "sqlite+aiosqlite:///:tasks.db"
+)
+
+new_session = async_sessionmaker(engine, expire_on_commit=False) # открытие транзакции БД
+
+class Model(DeclarativeBase):
+    pass
+
+class TableOrm(Model): # Объект модели
+    __tablename__ = "tasks" # имя таблицы
+
+    id: Mapped[int] = mapped_column(primary_key=True) # первичный ключ
+    name: Mapped[str]
+    description: Mapped[str | None]
+
+async def create_tables(): # функция для создания таблиц
+    async with engine.begin() as conn: # обращаться к engine
+        await conn.run_sync(Model.metadata.create_all) # создает все таблицы
+
+async def delete_tables(): # функция для удаления всех таблиц
+    async with engine.begin() as conn: # обращаться к engine
+        await conn.run_sync(Model.metadata.drop_all) # создает все таблицы
